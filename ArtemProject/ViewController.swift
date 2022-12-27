0e9ac1982a691fd69b0f6258d934fcdccb38237a
Vava666 <vavilka666@gmail.com>
2022-12-27 21:23:09 +0400

    feat: добавил сетап

--- ArtemProject/ViewController.swift
+++ ArtemProject/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello")
+        setup()
     }
-
-
+    
+    func setup() {
+        
+    }
 }
 
